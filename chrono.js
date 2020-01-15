be49108d878f019e1bafec70f23648f42cf5c2d8
Pablolo <pablo_olondriz@hotmail.com>
2020-01-15 12:06:20 +0100

    starting

---
+++ chrono.js
@@ -0,0 +1,35 @@
+// Cronometro en consola con start/stop/reset, que salga el tiempo con minutos y segundos
+// con una Class
+// metodo showtime dentro de la clase para que me muestre el tiempo que tiene en ese momento
+
+class Timer {
+  constructor {
+    this.seconds = seconds;
+    this.minutes = minutes;
+  }
+  start() {
+
+  }
+  stop() {
+
+
+    //clear interval para pararlo
+  }
+  reset() {
+
+  }
+  showTime() {
+
+  }
+}
+
+let myTimer = new Timer();
+
+myTimer.stop();
+myTimer.start();
+myTimer.reset();
+myTimer.start();
+
+myTimer.showTime();
+myTimer.showTime();
+myTimer.showTime();
