77dd65261f5b542c430266383a768a5ece3fbb9a
Pablolo <pablo_olondriz@hotmail.com>
2020-01-16 19:02:42 +0100

    hours added

--- chrono.js
+++ chrono.js
@@ -1,19 +1,24 @@
 // Cronometro en consola con start/stop/reset, que salga el tiempo con minutos y segundos
 // metodo showtime dentro de la clase para que me muestre el tiempo que tiene en ese momento
-
+ 
 class Timer {
   constructor() {
     this.seconds = 0;
     this.minutes = 0;
+    this.hours = 0;
     this.intervalId = 0;
   }
   start() {
     this.intervalId = setInterval(() => {
-      console.log(`${this.minutes} minutes ${this.seconds} seconds`);
+      console.log(`${this.hours} hours ${this.minutes} minutes ${this.seconds} seconds`);
       this.seconds ++; 
       if (this.seconds === 60) {
         this.seconds = 0;
         this.minutes += 1;
+      }
+      if (this.minutes === 60) {
+        this.minutes = 0;
+        this.hours += 1;
       }
     }, 1000);
   }
@@ -26,10 +31,11 @@ class Timer {
     clearInterval(this.intervalId);
     this.seconds = 0;
     this.minutes = 0;
+    this.hours = 0;
     myTimer.start();
   }
   showTime() {
-    alert(`The timer is right now on ${this.minutes} minutes ${this.seconds} seconds`);
+    alert(`The timer is right now on ${this.hours} hours ${this.minutes} minutes ${this.seconds} seconds`);
   }
 }
 
